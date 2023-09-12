1578266f98853f703bcb1e5f0bc9e204abf0ce08
Naveen <bsa.naveenbabu@gmail.com>
2023-09-12 12:48:00 +0530

    Add login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { HttpHeaders } from '@angular/common/http';
+import { catchError, Observable,map } from 'rxjs';
+import { HttpClient} from '@angular/common/http'
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http:HttpClient,
+    private processHTTPMsgService : ProcessHTTPMsgService) {
+
+     }
+     sendLoginUser(message: message): Observable<message> {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json'
+        })
+      };
+      return this.http.post<message>(baseURL + 'LoginUser',message, httpOptions)
+        .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
 }
