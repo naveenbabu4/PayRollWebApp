04304473409db42274b468b53f45457ea94b08e6
Sai Kiran <saikiran.gosangi@gmail.com>
2023-09-13 16:01:07 +0530

    add employee

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AddEmployee } from 'src/app/models/add-employee.model';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
-export class AddEmployeeComponent {
+export class AddEmployeeComponent implements OnInit {
+
+  addEmployeeForm!: FormGroup;
+  addEmployee!: AddEmployee;
+  errMessage!: string;
+  
+  constructor(private fb: FormBuilder, private adminService: AdminService) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.addEmployeeForm = this.fb.group({
+      FirstName: "",
+      LastName: "",
+      Email: "",
+      Address: "",
+      Password: "",
+      PhoneNumber: "",
+      JoiningDate: "",
+      Position: ""
+    });
+  }
+  async AddEmployee() {
+    this.addEmployee = this.addEmployeeForm.value;
+    console.log(this.addEmployee);
+    (await this.adminService.AddEmployee(this.addEmployee))
+      .subscribe(
+        addEmployee => { this.addEmployee = addEmployee, console.log(addEmployee) },
+        errMessage => { this.addEmployee = <any>null; this.errMessage = <any>errMessage; }
+      )
+    this.addEmployeeForm.reset();
+  }
+
 
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -5,6 +5,7 @@ import { ProcessHTTPMsgService } from './process-httpmsg.service';
 import { HttpHeaders } from '@angular/common/http';
 import { baseURL } from '../shared/baseurl';
 import { catchError, Observable,map } from 'rxjs';
+import { AddEmployee } from '../models/add-employee.model';
 
 
 @Injectable({
@@ -22,4 +23,16 @@ export class AdminService {
     return this.http.post<Allowance>(baseURL + 'AdminController/AddClass',allowance, httpOptions)
       .pipe(catchError(this.processHTTPMsgService.handleError));
   }
+
+
+  async AddEmployee(addEmployee: AddEmployee):Promise<Observable<AddEmployee>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.post<AddEmployee>(baseURL + 'AdminController/CreateEmployee',addEmployee, httpOptions)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+ 
 }
